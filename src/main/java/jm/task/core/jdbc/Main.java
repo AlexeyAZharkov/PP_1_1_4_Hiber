53be8ddf83f556162aab74fcba631d48bb9f39a1
AlexeyAZharkov <azharkov@mail.ru>
2022-08-12 16:12:12 +0300

    12-08

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,57 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь  dd s
+        Util util = new Util();
+        User user1 = new User("Alex", "Fox", (byte) 33);
+
+        String query =  "CREATE SCHEMA `zhtable`;";
+        String query2 =  "CREATE TABLE `new_schema2`.`new_table` (\n" +
+                "  `id` INT NOT NULL,\n" +
+                "  `name` VARCHAR(45) NOT NULL,\n" +
+                "  `lastName` VARCHAR(45) NOT NULL,\n" +
+                "  `age` INT NOT NULL,\n" +
+                "  PRIMARY KEY (`id`),\n" +
+                "  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);";
+
+        try {
+            Statement statement = util.getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+//        final String URL = "jdbc:mysql://localhost:3306/test1";
+//        final String USER = "root";
+//        final String PASSWORD = "Root123456^";
+//        Connection connection;
+
+//        Driver driver;
+//        {
+//            try {
+//                driver = new com.mysql.cj.jdbc.Driver();
+//                DriverManager.registerDriver(driver);
+////                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+////                if (!connection.isClosed()) {
+////                    System.out.println("Соединение установлено");
+////                }
+////                System.out.printf("Conne");
+//            } catch (SQLException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+//
+//        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+//             Statement statement = connection.createStatement()) {
+//
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
+    private final String URL = "jdbc:mysql://localhost:3306/test1";
+    private final String USER = "root";
+    private final String PASSWORD = "Root123456^";
+    private Connection connection;
+
+    public  Util() {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
     // реализуйте настройку соеденения с БД
 }
